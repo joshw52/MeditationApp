31d7f7b129b5c97031b22faaf31fc265a5616b32
Joshua Williams <joshuawilliams@Joshuas-iMac.local>
2017-08-22 21:44:46 -0400

    query inserts for account

--- meditation_app/meditateServer.js
+++ meditation_app/meditateServer.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var path = require('path');
 var parser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
 
 var app = express();
+var port = 8080;
+var db = 'meditation';
+var url = 'mongodb://localhost:27017/' + db;
 
-app.set('port', 8080);
+app.set('port', port);
 
 app.use(express.static(path.join(__dirname + '/public')));
 
@@ -40,7 +44,57 @@ app.post('/meditationEntry', function(req, res) {
 });
 
 app.post('/accountCreation', function(req, res) {
-    res.sendFile(path.join(__dirname+'/login.html'));
+	var isErr = false;
+	
+	// Validate first and last name
+	if (String(req.body.firstname).length > 0 && String(req.body.lastname).length > 0) {
+	
+	} else {
+		isErr = true;
+	}
+	
+	// Make sure passwords match
+	if (String(req.body.password) !== String(req.body.confirmPass)) {
+		isErr = true;
+	}
+	
+	// Make sure username isn't taken
+	mongo.connect(url, function(err, db) {
+		db.collection('users').find({
+			username: req.body.username
+		}).toArray(function(err, docs) {
+			if (docs) isErr = true;
+		});
+	});
+	
+	// Make sure zip code is 5 digits
+	if (Number(req.body.zipcode) && String(req.body.zipcode).length === 5) {
+	
+	} else {
+		isErr = true;
+	}
+	
+
+	if (isErr === false) {
+		// Put the fields into an object
+		var user = {
+			firstname: req.body.firstname,
+			lastname: req.body.lastname,
+			username: req.body.username,
+			password: req.body.password,
+			zipcode: req.body.zipcode
+		}
+	
+		mongo.connect(url, function(err, db) {
+			db.collection('users').insert(user, function(err, docs) {
+				db.close();
+			});
+		});
+	
+	    res.sendFile(path.join(__dirname+'/login.html'));
+	} else {
+		console.log("There was an error in your query");
+	}
 })
 
 app.listen(8080);
