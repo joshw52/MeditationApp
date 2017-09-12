cb414438d69977d4a48d6fcd71f354a5b85d90e6
EC2 Default User <ec2-user@ip-172-31-16-197.us-east-2.compute.internal>
2017-09-12 19:08:31 +0000

    directory changes

---
+++ meditateServer.js
@@ -0,0 +1,418 @@
+var express = require('express');
+var path = require('path');
+var parser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
+var OID = require('mongodb').ObjectID;
+var session = require('client-sessions');
+
+var sessionUser = "";
+
+var app = express();
+/* https://www.npmjs.com/package/socket.io */
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var port = 8080;
+var db = 'meditation';
+var url = 'mongodb://localhost:27017/' + db;
+
+app.set('port', port);
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+// https://stormpath.com/blog/everything-you-ever-wanted-to-know-about-node-dot-js-sessions
+app.use(session({
+	cookieName: 'session',
+	secret: 'thisisthesecretkey',
+	duration: (1000 * 60 * 60),
+	activeDuration: (1000 * 60 * 30)
+}));
+
+// EJS is the view engine
+app.set('view engine', 'ejs');
+
+// Go to the home page, or redirect to the login if a user isn't logged in
+app.get('/home', function(req, res) {
+	if (req.session.user)
+		res.render('index');
+	else
+		res.render('login', { lerr: false, accountCreated: false });
+});
+
+// Get the records for a user for a month/year
+function getDates(user, month, year, callback) {
+	// Put the month and year in the proper format
+	var monthYear = String(year) + "-";
+	if (month < 10) monthYear += "0";
+	monthYear += String(Number(month)+1);
+
+	// Find the records for that month/year
+	mongo.connect(url, function(err, db) {
+		db.collection('meditationrecord').find({
+			username: {
+				$eq: user
+			},
+			date: new RegExp(monthYear)
+		}).toArray(function(err, docs) {
+			// Get the calendar
+			var dates = printCalendar(month, year, docs);
+			callback(dates);
+			db.close();
+		});
+	});
+}
+
+// Construct the calendar to be displayed for a particular month/year,
+// showing the meditation time, a link to modify the journal entry,
+// and a button to delete the entry
+function printCalendar(month, year, dates) {
+	var cal = "<table id='progressCalendar'><thead><tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr><tbody>";
+
+	// Get the number of days in the month
+	var numDays = 0;
+	switch (Number(month)) {
+		case 0:
+		case 2:
+		case 4:
+		case 6:
+		case 7:
+		case 9:
+		case 11:
+			numDays = 31;
+			break;
+		case 3:
+		case 5:
+		case 8:
+		case 10:
+			numDays = 30;
+			break;
+		case 1:
+			numDays = 28;
+			if (((Number(year) % 4 === 0) && (Number(year) % 100 !== 0)) || (Number(year) % 400 === 0)) numDays++;
+	}
+	
+	var firstDay = new Date(year, month);
+	var start = 0 - firstDay.getDay();
+	var end = numDays;// + (7 - Math.abs(start));
+	while ((end + Math.abs(start)) % 7 !== 0) end++;
+	
+	// For each day of the month, if there's an entry, put it in
+	var count = 0;
+	for (var d = start+1; d <= end; d++) {
+		if (count === 0) cal += "<tr>";
+		cal += "<td>";
+
+		// Put the day of the month in 
+		if (d > 0 && d <= numDays)
+			cal += "<div id='monthDay'>" + String(d) + "</div><br><div id='dayRecord'>";
+
+		// Put the meditation entries in
+		for (logs in dates) {
+			var loggedDay = String(dates[logs].date).split("-")[2];
+			if (Number(loggedDay) === d) {
+				cal += "<div id='logResult'>" + String(dates[logs].time);
+				cal += "&nbsp<form method='POST' action='journal' class='journalMods'>";
+				cal += "<input type='hidden' value='" + dates[logs].date + "' name='jdate'>";
+				cal += "<input type='hidden' value='" + dates[logs]._id + "' name='jid'>";
+				cal += "<input type='hidden' value='" + dates[logs].entry + "' name='jentry'>";
+				cal += "<button type='submit' id='editJournal'><i class='fa fa-book' aria-hidden='true'></i></button></form>";
+				cal += "&nbsp<form method='POST' action='deleteJournalEntry' class='journalMods' onsubmit='return confirm(\"Confirm that you wish to delete this meditation entry\")'>";
+				cal += "<input type='hidden' value='" + dates[logs]._id + "' name='jdid'>";
+				cal += "<button type='submit' id='deleteJournal'><i class='fa fa-times-circle' aria-hidden='true'></i></button></form></div>";
+			}
+		}
+
+		cal += "</div></td>";
+
+		count++;
+		if (count > 6) {
+			count = 0;
+			cal += "</tr>";
+		}
+	}
+	
+	cal += "</tbody></table>";
+
+	// Return the constructed calendar
+	return cal;
+}
+
+// Get the progress page, or redirecting to the login page
+// if a user isn't logged in
+app.get('/progress', function(req, res) {
+	if (req.session.user) {
+		var d = new Date();
+		var month = Number(d.getMonth());
+		var year = Number(d.getFullYear());
+		
+		getDates(req.session.user, month, year, function(dates) {
+			if (dates) res.render('progress', { progCal: dates });
+			else return;
+		});
+	}
+	else {
+		res.render('login', { lerr: false, accountCreated: false });
+	}
+});
+
+// Display the calendar of progress for a month/year
+app.post('/progress', function(req, res) {	
+	res.render('progress');
+});
+
+// If a journal is to be modified, put in the id and entry
+app.post('/journal', function(req, res) {
+	res.render('journal', {
+		jid: req.body.jid,
+		jentry: req.body.jentry,
+		jdate: req.body.jdate
+	});
+});
+
+// Modify the journal entry in the collection, using the id to find it
+app.post('/journalModification', function(req, res) {	
+	var jeid = new OID(req.body.mjid);
+	
+	// Update the record
+	mongo.connect(url, function(err, db) {
+		db.collection('meditationrecord').update(
+			{ _id: jeid },
+			{ $set:
+				{ entry: req.body.mjentry }
+			},
+			function() {
+				db.close();
+			}
+		);
+	});
+	
+	res.render('progress', { progCal: "Meditation Entry Modified!" });
+});
+
+// Delete a journal entry
+app.post('/deleteJournalEntry', function(req, res) {
+	var jdid = new OID(req.body.jdid);
+	
+	// Delete the record
+	mongo.connect(url, function(err, db) {
+		db.collection('meditationrecord').remove({
+			_id: jdid
+		}, function() {
+			db.close();
+		});
+	});
+	
+	res.render('progress', { progCal: "Meditation Entry Deleted!" });
+});
+
+/* Logout */
+
+// Log out of the site
+app.get('/logout', function(req, res) {
+	req.session.reset();
+	sessionUser = "";
+	res.render('login', { lerr: false, accountCreated: false });
+});
+
+/* Timer code */
+
+// Get the timer page, redirect to login if no user is logged in
+app.get('/timer', function(req, res) {
+	if (req.session.user) {
+		mongo.connect(url, function(err, db) {
+			db.collection('users').findOne({
+				username: req.session.user
+			}, function(err, time) {
+				if (time === null) {
+					res.render('timer', {
+						defaultHrs: "0",
+						defaultMinutes: "10",
+						defaultSeconds: "00"
+					});
+				}
+				else {
+					res.render('timer', {
+						defaultHrs: time.defaultHrs,
+						defaultMinutes: time.defaultMinutes,
+						defaultSeconds: time.defaultSeconds
+					});
+				}		
+				db.close();
+			});
+		});
+	}
+	else {
+		res.render('login', { lerr: false, accountCreated: false});
+	}
+});
+
+// Show the meditation timer
+app.post('/timer', function(req, res) {
+	// Put the date in the proper format
+	var dt = new Date();
+	var m = dt.getMonth() + 1;
+	if (m < 10) m = "0" + String(m);
+	var d = dt.getDate();
+	if (d < 10) d = "0" + String(d);
+	
+	var entryDate = dt.getFullYear() + "-" + m + "-" + d;
+	
+	// The meditation log for a user
+	var mlog = {
+		username: req.session.user,
+		date: entryDate,
+		time: req.body.meditationTime,
+		entry: req.body.journalEntry
+	}
+	
+	// Insert the meditation log to the database
+	mongo.connect(url, function(err, db) {
+		db.collection('meditationrecord').insert(mlog, function(err, docs) {
+			if (err) console.log(err);
+			else console.log("Entry made\n");
+			
+			db.close();
+		});
+	});
+	
+	// Move to the progress page
+	res.render('progress', { progCal: "Meditation Entry Made!" });
+});
+
+/* Set up the account */
+
+// Sockets for checking the username for the account creation
+// as well as the username/password for the login
+io.on('connection', function(sock) {
+	console.log('Client connected...');
+	
+	sock.on('requestMonthProgress', function(date) {	
+		getDates(sessionUser, Number(date.progressMonth), Number(date.progressYear), function(dates) {
+			sock.emit('receiveMonthProgress', { progDates: dates });
+		});
+	});
+	
+	// Set the default time for a user
+	sock.on('setDefaultTime', function(time) {
+		
+		mongo.connect(url, function(err, db) {
+			db.collection('users').update(
+				{ username: sessionUser },
+				{ $set:
+					{
+						defaultHrs: time.defaultHrs,
+						defaultMinutes: time.defaultMinutes,
+						defaultSeconds: time.defaultSeconds,
+					}
+				}, function() {
+					console.log("Default time modified...");
+					db.close();
+				}
+			);
+		});
+	});
+	
+	// Check to see if the username is available, sending a
+	// signal back to the client if it is or isn't
+	sock.on('unameCheck', function(uname) {		
+		mongo.connect(url, function(err, db) {
+			db.collection('users').findOne({
+				username: uname.username
+			}, function(err, item) {
+				if (item === null) {
+					console.log("Username available\n");
+					sock.emit('unameCheckResponse', { unameFree: true });
+				} else {
+					console.log("Username not available\n");
+					sock.emit('unameCheckResponse', { unameFree: false });
+				}		
+				db.close();
+			});
+		});
+	});
+	
+	// Check that the login credentials are correct, 
+	// that the username exists and that the password is correct
+	sock.on('loginCheck', function(login) {
+		mongo.connect(url, function(err, db) {		
+			// Look for username
+			db.collection('users').findOne({
+				username: login.loginUname,
+			}, function(err, item) {
+				
+				// If the username is not found or the login password doesn't match the user's password
+				if (!item) {
+					console.log("The username is not valid\n");
+					sock.emit('loginCheckResponse', { loginAccepted: false });
+				} 
+				else {
+					if (login.loginPword !== item.password) {
+						console.log("The password is not correct\n");
+						sock.emit('loginCheckResponse', { loginAccepted: false });
+					} 
+					// Indicate if the credentials are correct
+					else {
+						console.log("The entry is correct!\n");
+						sock.emit('loginCheckResponse', { loginAccepted: true });
+					}
+				}
+				db.close();
+			});
+		});
+	});
+});
+
+// Render the account creation page
+app.get('/account', function(req, res) {
+	res.render('account');
+});
+
+// Add an account to the users collection
+app.post('/account', function(req, res) {
+	console.log("User added\n");
+
+	// The user entry to be made
+	var user = {
+		firstname: req.body.firstname,
+		lastname: req.body.lastname,
+		username: req.body.username,
+		password: req.body.password,
+		email: req.body.email,
+		zipcode: req.body.zipcode,
+		defaultHrs: "0",
+		defaultMinutes: "10",
+		defaultSeconds: "00"
+	}
+
+	// Insert the account
+	mongo.connect(url, function(err, db) {
+		db.collection('users').insert(user, function(err, docs) {
+			db.close();
+		});
+	});
+	
+	// Move to the login page
+	res.render('login', { lerr: false, accountCreated: true });
+})
+
+/* Log in */
+
+// Move to the login page
+app.get('/', function(req, res) {
+	res.render('login', { lerr: false, accountCreated: false });
+});
+
+// The logged in user will move to the home page
+app.post('/home', function(req, res) {
+	req.session.user = req.body.loginUname;
+	sessionUser = req.session.user;
+	res.redirect('/home');
+});
+
+// Listen for an incoming connection
+server.listen(8080, function() {
+	console.log("Server is listening...\n");
+});
---
+++ public/meditation.js
@@ -0,0 +1,378 @@
+var timerOn = null;	// Indicates if timer is running or not
+var timeMeditated = -1;	// Hold the time meditated
+var incrementFraction = -1;	// The fraction that the image will be revealed in per second while the timer runs
+var currentBrightness = 0;	// The current brightness of the namaste image
+var gong = new Audio('https://soundbible.com/grab.php?id=1815&type=mp3');	// The gong sound to be played at the beginning and end
+
+// Log in
+function loginMeditation(event) {
+	// Create a socket
+	var socket = io();
+
+	// Send the username/password to the server
+	socket.emit('loginCheck', {
+		loginUname: document.getElementById('loginUname').value,
+		loginPword: document.getElementById('loginPword').value
+	});
+	
+	// check if the server approved of the username/password combo
+	socket.on('loginCheckResponse', function(response) {
+		// If the login was accepted, login to the site
+		if (response.loginAccepted === true) {
+			// Now the form can be submitted
+			document.getElementById('loginForm').submit();
+		}
+		// If the login was not accepted, display an error
+		else {
+			document.getElementById('loginErr').innerHTML = "<br><span style='color: #FF5555;'>Username/password credentials are incorrect!</span>";
+		}
+	});
+	
+	// The form will not submit unless the login was accepted
+	event.preventDefault();
+}
+
+function setDefaultTime() {
+	// Create a socket
+	var socket = io();
+
+	// Send the username/password to the server
+	socket.emit('setDefaultTime', {
+		defaultHours: document.getElementById('adjustHr').innerHTML,
+		defaultMinutes: document.getElementById('adjustMin').innerHTML,
+		defaultSeconds: document.getElementById('adjustSec').innerHTML		
+	});
+}
+
+// Will send the chosen month/year to the server to retrieve the
+// progress for that time period
+function displayProgress() {
+	var socket = io();
+	
+	socket.emit('requestMonthProgress', {
+		progressMonth: document.getElementById('progressMonth').value,
+		progressYear: document.getElementById('progressYear').value
+	});
+	
+	socket.on('receiveMonthProgress', function(progress) {
+		document.getElementById('progDiv').innerHTML = progress.progDates;
+	});
+}
+
+// Create an account
+function createAccount(event) {	
+	document.getElementById('errMsg').innerHTML = "";
+	document.getElementById('errMsg').style.display = "none";
+	
+	var emptyField = false;
+	var mismatchedPasswords = false;
+	var shortPasswords = false;
+	var zipCodeWrongLength = false;
+	var errMessage = "";
+
+	// Validate first name (can't be empty)
+	if (document.getElementById('firstname').value === "" || document.getElementById('lastname').value === "" || 
+		document.getElementById('username').value === "" || document.getElementById('password').value === "" ||
+		document.getElementById('confirmPass').value === "" || document.getElementById('email').value === "" ||
+		document.getElementById('zipcode').value === "") {
+		emptyField = true;
+		errMessage += "All fields must be filled!<br>";
+	}
+	
+	// Validate the passwords (make sure at least 8 characters)
+	if (String(document.getElementById('password').value).length < 8 || String(document.getElementById('confirmPass').value).length < 8) {
+		shortPasswords = true;
+		errMessage += "Passwords must be at least 8 characters!<br>";
+	}
+	
+	// Make sure the passwords match
+	if (document.getElementById('password').value !== document.getElementById('confirmPass').value) {
+		mismatchedPasswords = true;
+		errMessage += "Passwords do not match!<br>";
+	}
+	
+	// Make sure zip code is 5 digits
+	if (String(document.getElementById('zipcode').value).length !== 5) {
+		zipCodeWrongLength = true;
+		errMessage += "Zip code must be 5 digits long!<br>";
+	}
+	
+	// The form will not submit if there were errors
+	if (emptyField === true || mismatchedPasswords == true || shortPasswords == true || zipCodeWrongLength == true) {
+		document.getElementById('errMsg').innerHTML = errMessage;
+		document.getElementById('errMsg').style.display = "block";
+		return false;
+	}
+	// Check if the username was chosen already first, and if not,
+	// then submit the account form
+	else {		
+		// Create a socket
+		var socket = io();
+
+		// Send the username to the server to see if it's been chosen
+		socket.emit('unameCheck', {
+			username: document.getElementById('username').value
+		});
+		
+		// The server will indicate if the username has already been picked
+		socket.on('unameCheckResponse', function(response) {			
+			// If the username has not already been picked
+			if (response.unameFree === true) {
+				// Now the account form can be submitted
+				document.getElementById('accountCreation').submit();
+			}
+			// If the username is being used, display error
+			else {
+				errMessage += "Username is taken!";	
+				document.getElementById('errMsg').innerHTML = errMessage;
+				document.getElementById('errMsg').style.display = "block";
+			}
+		});
+	};
+	
+	// Make sure the form doesn't submit
+	event.preventDefault();
+}
+
+// Increments or decrements the hour
+function modifyHour(sign) {
+	// Don't modify if the timer is running
+	if (timerOn === null) {
+		// Get the current hour
+		var hour = document.getElementById("adjustHr").innerHTML;
+		
+		// Increment
+		if (sign === 1) {
+			document.getElementById("adjustHr").innerHTML = String(Number(hour) + 1);
+		}
+		// Decrement if greater than 0
+		else if (sign === -1 && document.getElementById("adjustHr").innerHTML > 0)  {
+			document.getElementById("adjustHr").innerHTML = String(Number(hour) - 1);
+		}
+	}
+}
+
+// Increments or decrements the minute
+function modifyMinute(sign) {
+	// Don't modify if the timer is running
+	if (timerOn === null) {
+		// Get the current minute
+		var minute = document.getElementById("adjustMin").innerHTML;
+		
+		// Increment
+		if (sign === 1) {
+			if (minute === "59") {
+				modifyHour(1);
+				document.getElementById("adjustMin").innerHTML = "0";				
+			} else {
+				document.getElementById("adjustMin").innerHTML = String(Number(minute) + 1);
+			}
+		}
+		// Decrement if greater than 0
+		else if (sign === -1) {
+			if (minute > 0) {
+				document.getElementById("adjustMin").innerHTML = String(Number(minute) - 1);
+			} else if (sign === -1 && minute == 0 && document.getElementById("adjustHr").innerHTML !== "0") {
+				modifyHour(-1);
+				document.getElementById("adjustMin").innerHTML = "59";
+			}	
+		}
+	}
+}
+
+// Increments or decrements the second
+function modifySecond(sign) {
+	// Don't modify if the timer is running
+	if (timerOn === null) {
+		// Get the current second
+		var second = document.getElementById("adjustSec").innerHTML;
+		
+		// Increment
+		if (sign === 1) {
+			if (second < 59) {
+				second = String(Number(second) + 1);
+			} else if (second === "59") {
+				modifyMinute(1);
+				second = "0";
+			}
+		}
+		// Decrement if greater than 00
+		else if (sign === -1) {
+			if (second > 0) {
+				second = String(Number(second) - 1);
+			} else if (second === "00") {
+				if (document.getElementById("adjustMin").innerHTML > 0) {
+					modifyMinute(-1);
+					second = "59";
+				} else if (document.getElementById("adjustMin").innerHTML == 0 && document.getElementById("adjustHr").innerHTML > 0) {
+					//modifyHour(-1);
+					modifyMinute(-1);
+					second = "59";
+				}
+			}
+		}
+		
+		// Make sure numbers less than 10 display a leading zero
+		if (second < 10 && String(second) !== "00") {
+			second = "0" + String(second);
+		}
+			
+		document.getElementById("adjustSec").innerHTML = String(second);
+	}
+}
+
+// This will end the session and prompt for a journal entry
+function endTimer() {
+	// Play the gong to indicate the meditation period is over
+	gong.play();
+	
+	// Put the time meditated into the form field
+	document.getElementById('meditationTime').value = timeMeditated;
+	
+	// Stop timer
+	stopTimer();
+	timeMeditated = -1;
+	incrementFraction = -1;
+	currentBrightness = 0;
+
+	gong.onended = function() {				
+		// Prompt the user to move to the journal entry or skip and submit
+		var journalEntry = confirm("Click OK to make a journal entry");
+	
+		if (journalEntry) {
+			// Hide the timer and display the journal entry
+			document.getElementById('meditationTimer').style.display = "none";
+			document.getElementById('meditationJournal').style.display = "";
+		} else {
+			document.getElementById("meditationEntry").submit();
+		}
+	};
+}
+
+// Start the timer
+function startTimer() {
+	// Log the total time the user is supposed to meditate in seconds
+	if (timeMeditated === -1 && incrementFraction === -1) {
+		timeMeditated = String(document.getElementById("adjustHr").innerHTML) + ":" + String(document.getElementById("adjustMin").innerHTML) + ":" + String(document.getElementById("adjustSec").innerHTML);
+		incrementFraction = 100 / (Number(document.getElementById("adjustHr").innerHTML)*60*60 + Number(document.getElementById("adjustMin").innerHTML)*60 + Number(document.getElementById("adjustSec").innerHTML));
+	}
+	
+	// Only start if the timer is not on & there are not values waiting to be modified
+	if (timerOn === null && document.getElementById("hrInput").style.display === "none" && document.getElementById("minInput").style.display === "none" && document.getElementById("secInput").style.display === "none") {
+		gong.play();
+		
+		document.getElementById('buddhaFill').style.opacity = 0.85;
+		document.getElementById('buddhaFillImg').style.display = "initial";
+		
+		// Start the timer, counting down every second
+		timerOn = setInterval(function() {
+			var hrs = document.getElementById("adjustHr").innerHTML;
+			var min = document.getElementById("adjustMin").innerHTML;
+			var sec = document.getElementById("adjustSec").innerHTML;
+
+			// If the timer is up
+			if (hrs === "0" && min === "0" && sec === "00") {
+				endTimer();
+				
+			} else {
+				// Brighten the center image
+				currentBrightness += incrementFraction;
+				var brightVar = "invert(" + String(currentBrightness) + "%)";
+				document.getElementById('buddhaFillImg').style.filter = brightVar;				
+				
+				// If seconds reach zero, decrement to 59
+				if (sec === "00") {
+					document.getElementById("adjustSec").innerHTML = "59";
+					
+					// Decrement minutes
+					if (min > 0) {
+						document.getElementById("adjustMin").innerHTML = String(min - 1);
+					}
+					
+					else {
+						// Adjust the hours
+						if (hrs > 0) {
+							document.getElementById("adjustHr").innerHTML  = String(hrs - 1);
+							document.getElementById("adjustMin").innerHTML = "59";
+						}
+					}
+				}
+				// Else decrement by 1
+				else {
+					sec = Number(sec) - 1;
+					
+					if (sec < 10) {
+						sec = "0" + String(sec);
+					}
+					
+					document.getElementById("adjustSec").innerHTML = sec;
+				}
+			}
+		}, 1000);
+	}
+}
+
+// Stop the timer
+function stopTimer() {
+	clearInterval(timerOn);
+	timerOn = null;
+}
+
+// Reset the timer
+function resetTimer() {
+	stopTimer();
+	location.reload();
+}
+
+function replaceHrVal(textId, inputId) {
+	if (textId === 'adjustHr' && Number(document.getElementById(inputId).value) >= 0) {
+		document.getElementById(textId).innerHTML = Number(document.getElementById(inputId).value);				
+		document.getElementById(inputId).style.display = 'none';
+		document.getElementById(textId).style.display = '';
+	}
+}
+
+// Allow the user to manually change the timer
+function changeTimeVal(textId, inputId) {
+	// Don't let the user modify the time if the timer is going
+	if (!timerOn) {
+		// Put the current value of the text in the input box
+		document.getElementById(inputId).value = document.getElementById(textId).innerHTML;
+	
+		// Hide the text and show the input, focus on it
+		document.getElementById(textId).style.display = 'none';
+		document.getElementById(inputId).style.display = '';
+		document.getElementById(inputId).focus();
+	
+		// When the user clicks enter, put the input value back into the text
+		document.getElementById(inputId).onkeypress = function(e) {
+			// Get the keyboard input
+			var isEnter = e.which || e.keyCode;
+		
+			// If the user clicked enter, put the value in the input box back into the text area
+			if (isEnter == '13') {
+				// Replace the hour text as long as the text is 0 or greater
+				if (textId === 'adjustHr') replaceHrVal(textId, inputId);
+				
+				// Enter the minute if within 0-59
+				else if (textId === 'adjustMin' && Number(document.getElementById(inputId).value) >= 0 && Number(document.getElementById(inputId).value) <= 59) {
+					document.getElementById(textId).innerHTML = Number(document.getElementById(inputId).value);
+					document.getElementById(inputId).style.display = 'none';
+					document.getElementById(textId).style.display = '';
+				}
+				// Enter the second if within 0-59, put trailing 0 in if less than 10
+				else if (textId === 'adjustSec' && Number(document.getElementById(inputId).value) >= 0 && Number(document.getElementById(inputId).value) <= 59) {
+					if (Number(document.getElementById(inputId).value) >= 10 && Number(document.getElementById(inputId).value) <= 59) {
+						document.getElementById(textId).innerHTML = Number(document.getElementById(inputId).value);			
+					} else if (Number(document.getElementById(inputId).value) >= 0 && Number(document.getElementById(inputId).value) < 10) {
+						var secVal = "0" + String(Number(document.getElementById(inputId).value));
+						document.getElementById(textId).innerHTML = secVal;			
+					}
+				
+					document.getElementById(inputId).style.display = 'none';
+					document.getElementById(textId).style.display = '';
+				}
+			}
+		}
+	}
+}
