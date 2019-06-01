a876a53e8b5dfce7d1d2c85c77ca55092457e54d
Joshua Williams <joshwilliams@Joshuas-MBP.lan1>
2019-06-01 02:33:34 -0700

    made some small changes

--- meditation-react/src/components/Meditate.js
+++ meditation-react/src/components/Meditate.js
@@ -7,10 +7,10 @@ class Meditate extends React.Component {
         super(props);
 
         this.state = {
+            journalView: false,
             meditateHours: '0',
             meditateMinutes: '10',
             meditateSeconds: '0',
-            timerEditing: false,
             timerInfoShow: false,
             timerRunning: false,
         };
@@ -19,12 +19,6 @@ class Meditate extends React.Component {
     displayTimerInfo = () => {
         this.setState(state => ({
             timerInfoShow: !state.timerInfoShow,
-        }));
-    }
-
-    toggleEditing = timeValue => {
-        this.setState(state => ({
-            [timeValue]: !state[timeValue],
         }));
     }
 
@@ -70,6 +64,7 @@ class Meditate extends React.Component {
                 if (totalTime <= 0) {
                     clearInterval(this.meditationTimer);
                     this.setState({
+                        journalView: true,
                         timerRunning: false,
                     });
                 } else {
@@ -97,82 +92,79 @@ class Meditate extends React.Component {
 
     render () {
         const {
+            journalView,
             meditateHours,
             meditateMinutes,
             meditateSeconds,
-            timerEditing,
             timerInfoShow,
             timerRunning
         } = this.state;
 
         return (
             <div>
-                <div className='meditationTimer'>
-                    <div className='timerAdjust'>
+                {journalView ?
+                    <div className='meditationJournal' style='display: none;'>
+                        {/* {/* <form action='/timer' method='POST' className='meditationEntry'> */}
+                        <h3>Log your meditation</h3>
+                        <textarea className='journalEntry' name='journalEntry' />
+                        <input type='submit' className='logJournal' value='Log Journal' />
+                    </div> :
+                    <div className='meditationTimer'>
+                        <div className='timerAdjust'>
+                            <div className='timerRow'>
+                                <input
+                                    className='timerInput'
+                                    disabled={timerRunning}
+                                    name='meditateHours'
+                                    onChange={this.modifyTime}
+                                    value={meditateHours}
+                                />
+                                <input
+                                    className='timerInput'
+                                    disabled={timerRunning}
+                                    name='meditateMinutes'
+                                    onChange={this.modifyTime}
+                                    value={meditateMinutes}
+                                />
+                                <input
+                                    className='timerInput'
+                                    disabled={timerRunning}
+                                    name='meditateSeconds'
+                                    onChange={this.modifyTime}
+                                    value={meditateSeconds}
+                                />
+                            </div>
+                        </div>
+
                         <div className='timerRow'>
-                            <input
-                                className='timerInput'
-                                disabled={timerRunning}
-                                name='meditateHours'
-                                onChange={this.modifyTime}
-                                value={meditateHours}
-                            />
-                            <input
-                                className='timerInput'
-                                disabled={timerRunning}
-                                name='meditateMinutes'
-                                onChange={this.modifyTime}
-                                value={meditateMinutes}
-                            />
-                            <input
-                                className='timerInput'
-                                disabled={timerRunning}
-                                name='meditateSeconds'
-                                onChange={this.modifyTime}
-                                value={meditateSeconds}
-                            />
+                            <button onClick={this.startTimer}>Start</button>
+                            <button onClick={this.stopTimer}>Stop</button>
+                            <button onClick={this.setDefaultTime}>Reset</button>
+                            <button className='info' onClick={this.displayTimerInfo}>
+                                <FontAwesomeIcon icon={faInfoCircle} />
+                            </button>
+                        </div>
+
+                        <div className='defaultTimeMsg'></div>
+
+                        {timerInfoShow &&
+                            <div className='timerInfo'>
+                                <p>Here you can record a meditation session.  A gong will sound both when the
+                                session starts and when it ends.  Press <span className='textSpan'>Start</span> to
+                                start the timer, <span className='textSpan'>Stop</span> to stop it, <span className='textSpan'>Reset</span> to
+                                go back to the beginning, or <span className='textSpan'>Set Default Time</span> to
+                                personalize your default meditation time.  You can adjust the time with either the up 
+                                and down arrows, or you can click on the numbers and enter in the appropriate time.
+                                You will be prompted to make a journal entry after the meditation session ends, where
+                                you can log your thoughts, or you can cancel to not record a journal entry.</p>
+                            </div>
+                        }
+
+                        <div className='buddhaFill'>
+                            <img className='buddhaFillImg' src='http://www.vagabondtemple.com/wp-content/uploads/2016/07/mantra-om-1292602.png' />
                         </div>
                     </div>
-
-                    <div className='timerRow'>
-                        <button onClick={this.startTimer}>Start</button>
-                        <button onClick={this.stopTimer}>Stop</button>
-                        <button onClick={this.setDefaultTime}>Reset</button>
-                        <button className='info' onClick={this.displayTimerInfo}>
-                            <FontAwesomeIcon icon={faInfoCircle} />
-                        </button>
-                    </div>
-
-                    <div className='defaultTimeMsg'></div>
-
-                    {timerInfoShow &&
-                        <div className='timerInfo'>
-                            <p>Here you can record a meditation session.  A gong will sound both when the
-                            session starts and when it ends.  Press <span className='textSpan'>Start</span> to
-                            start the timer, <span className='textSpan'>Stop</span> to stop it, <span className='textSpan'>Reset</span> to
-                            go back to the beginning, or <span className='textSpan'>Set Default Time</span> to
-                            personalize your default meditation time.  You can adjust the time with either the up 
-                            and down arrows, or you can click on the numbers and enter in the appropriate time.
-                            You will be prompted to make a journal entry after the meditation session ends, where
-                            you can log your thoughts, or you can cancel to not record a journal entry.</p>
-                        </div>
-                    }
-
-                    <div className='buddhaFill'>
-                        <img className='buddhaFillImg' src='http://www.vagabondtemple.com/wp-content/uploads/2016/07/mantra-om-1292602.png' />
-                    </div>
-                </div>
-                
-                {/* <div className='meditationJournal' style='display: none;'>
-                    <form action='/timer' method='POST' className='meditationEntry'>
-                        <h3>Log your meditation</h3><br>
-                        <input type='hidden' className='meditationTime' value='' name='meditationTime'>
-                        <input type='hidden' className='meditationDate' value='' name='meditationDate'>
-                        <input type='hidden' className='meditationHrMin' value='' name='meditationHrMin'>
-                        <textarea className='journalEntry' name='journalEntry'></textarea><br>
-                        <input type='submit' className='logJournal' value='Log Journal'>
-                    </form>
-                </div> */}
+                }
             </div>
         );
     }
