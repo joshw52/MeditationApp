a0f68811ebb549b9f45dfe46093354567a43e27a
Joshua Williams <joshwilliams@Joshuas-MBP.lan1>
2019-06-12 22:57:10 -0700

    changed api

--- client/src/components/CreateAccount.js
+++ client/src/components/CreateAccount.js
@@ -55,7 +55,7 @@ class CreateAccount extends React.Component {
                 accountError: "Password must be at least 8 characters",
             });
         } else {
-            axios.post("localhost:8080/account", {
+            axios.post("/account", {
                 accountEmail,
                 accountFirstName,
                 accountLastName,
