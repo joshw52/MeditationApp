1daab01a33948131761235d7a94102314d72a58e
Joshua Williams <joshwilliams@Joshuas-MBP.lan1>
2019-06-12 23:47:11 -0700

    fixed url

--- meditateServer.js
+++ meditateServer.js
@@ -21,8 +21,7 @@ function encrypt(str) {
 
 var port = process.env.PORT || 8080;
 var db = 'meditation';
-var url = (process.env.MONGODB_URI || 'mongodb://localhost:27017/') + db;
-console.log("url: ", url, "\n\n\n")
+var url = (process.env.MONGODB_URI || 'mongodb://localhost:27017') + "/" + db;
 
 var userLogin = '';
 
