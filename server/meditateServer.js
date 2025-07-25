f3994b724e1eab291d7982cbc3f0f0d762aa8881
Josh <jshwilliams26@gmail.com>
2025-07-24 20:39:42 -0700

    Refactored endpoint code, added session middleware

--- server/meditateServer.js
+++ server/meditateServer.js
@@ -64,30 +64,38 @@ app.get('/api/isAuthenticated', function(req, res) {
 	res.json({ isAuthenticated: !!isAuthenticated });
 });
 
-app.post('/api/account', async function(req, res) {
-	// The user entry to be made
-	const newUser = {
-		username: req.body.accountUsername,
-		password: encrypt(req.body.accountPassword),
-		email: req.body.accountEmail,
-		defaultMeditationTime: 600,
-	}
+const requireLogin = (req, res, next) => {
+  if (req.session && req.session.username) {
+    next();
+  } else {
+    res.status(401).json({ error: "Not authenticated" });
+  }
+};
 
-	// Server-side validation of non-empty fields
-	if (!newUser.username || !newUser.password || !newUser.email) {
-		res.json({
-			accountCreated: false,
-			accountMsg: "All fields must be filled out!",
-		});
-	// Server-side validation of password length
-	} else if (newUser.password.length < 8) {
-		res.json({
-			accountCreated: false,
-			accountMsg: "Password must be at least 8 characters",
-		});
-	} else {
-		// Attempt to insert the account
-		try {
+app.post('/api/account', async function(req, res) {
+	try {
+		// The user entry to be made
+		const newUser = {
+			username: req.body.accountUsername,
+			password: encrypt(req.body.accountPassword),
+			email: req.body.accountEmail,
+			defaultMeditationTime: 600,
+		}
+
+		// Server-side validation of non-empty fields
+		if (!newUser.username || !newUser.password || !newUser.email) {
+			res.json({
+				accountCreated: false,
+				accountMsg: "All fields must be filled out!",
+			});
+		// Server-side validation of password length
+		} else if (newUser.password.length < 8) {
+			res.json({
+				accountCreated: false,
+				accountMsg: "Password must be at least 8 characters",
+			});
+		} else {
+			// Attempt to insert the account
 			const item = await db.collection('users').findOne({
 				username: req.body.accountUsername,
 			});
@@ -98,21 +106,21 @@ app.post('/api/account', async function(req, res) {
 					accountMsg: "Username is already taken",
 				});
 			} else {
-				const item = db.collection('users').insertOne(newUser);
-
-				if (item) {
-					res.json({
-						accountCreated: true,
-						accountMsg: '',
-					});
-				}
+				db.collection('users').insertOne(newUser);
+				res.json({
+					accountCreated: true,
+					accountMsg: '',
+				});
 			}
-		} catch (err) {
-			console.error(err);
-			res.status(500).end();
 		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			accountCreated: false,
+			accountMsg: '',
+		});
 	}
-})
+});
 
 // Check that the login credentials are correct, 
 // that the username exists and that the password is correct
@@ -137,7 +145,9 @@ app.post('/api/login', async (req, res) => {
 		}
 	} catch (err) {
 		console.error(err);
-		res.status(500).end();
+		res.status(500).json({
+			loginAccepted: false,
+		});
 	}
 });
 
@@ -147,56 +157,49 @@ app.post('/api/userLogout',(req, res) => {
 	res.end();
 });
 
-app.get('/api/meditationTime', function(req, res) {
-	if (req.session.username) {
-		mongo.connect(mongoDBUrl, function(err, client) {
-			const db = client.db(database);	
-			db.collection('users').findOne({
-				username: req.session.username,
-			}, function(err, item) {
-				if (err) throw err;
-
-				if (item) {
-					res.json({
-						defaultMeditationTime: item.defaultMeditationTime,
-					});
-				}
+app.get('/api/meditationTime', requireLogin, function(req, res) {
+	try {
+		const item = db.collection('users').findOne({
+			username: req.session.username,
+		});
+		if (item) {
+			res.json({
+				defaultMeditationTime: item.defaultMeditationTime,
 			});
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			defaultMeditationTime: null,
 		});
 	}
 });
 
-app.post('/api/meditationTime', function(req, res) {
-	if (req.session.username) {
-		mongo.connect(mongoDBUrl, function(err, client) {
-			const db = client.db(database);	
-			db.collection('users').findOne({
-				username: req.session.username,
-			}, function(err, item) {
-				if (err) throw err;
-
-				if (item) {
-					db.collection('users').findOneAndUpdate(
-						{ _id: item._id },
-						{ $set: {
-							defaultMeditationTime: req.body.defaultMeditationTime,
-						}},
-						function(err, updatedItem) {
-							if (err) throw err;
-
-							res.json({
-								defaultMeditationTime: updatedItem.value.defaultMeditationTime,
-							});
-						}
-					);
-				}
+app.post('/api/meditationTime', requireLogin, function(req, res) {
+	try {
+		const item = db.collection('users').findOne({
+			username: req.session.username,
+		});
+		if (item) {
+			const updatedItem = db.collection('users').findOneAndUpdate(
+			{ _id: item._id },
+			{ $set: {
+				defaultMeditationTime: req.body.defaultMeditationTime,
+			}});
+			res.json({
+				defaultMeditationTime: updatedItem.value.defaultMeditationTime,
 			});
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			defaultMeditationTime: null,
 		});
 	}
 });
 
-app.post('/api/meditationEntry', function(req, res) {
-	if (req.session.username) {
+app.post('/api/meditationEntry', requireLogin, function(req, res) {
+	try {
 		const meditationEntry = {
 			username: req.session.username,
 			meditateDateTime: req.body.meditateDateTime,
@@ -204,180 +207,158 @@ app.post('/api/meditationEntry', function(req, res) {
 			journalEntry: req.body.journalEntry
 		}
 
-		// Insert the meditation entry to the database
-		mongo.connect(mongoDBUrl, function(err, client) {
-			const db = client.db(database);	
-			if (err) throw err;
-			
-			db.collection('meditationrecord').insertOne(meditationEntry, function(err, docs) {
-				if (err) throw err;
-
-				res.json({
-					meditationEntryMsg: 'Meditation Entry Made!',
-				});
-			});
+		// Insert the meditation entry to the database		
+		db.collection('meditationrecord').insertOne(meditationEntry);
+		res.json({
+			meditationEntryMsg: 'Meditation Entry Made!',
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			meditationEntryMsg: 'Error making meditation entry',
 		});
 	}
 });
 
-app.get('/api/accountInfoLoad', function(req, res) {
-	if (req.session.username) {
-		mongo.connect(mongoDBUrl, function(err, client) {
-			const db = client.db(database);	
-			if (err) throw err;
-			db.collection('users').findOne({
-				username: req.session.username
-			}, function(err, user) {
-				if (err) throw err;
-
-				res.json({ email: user.email });
-			});
+app.get('/api/accountInfoLoad', requireLogin, function(req, res) {
+	try {
+		const user = db.collection('users').findOne({
+			username: req.session.username
+		});
+		res.json({ email: user.email });
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			email: null,
 		});
 	}
 });
 
-app.post('/api/accountModify', function(req, res) {
-	if (req.session.username) {
+app.post('/api/accountModify', requireLogin, function(req, res) {
+	try {
 		// Modify the account if the user clicked Modify and not Cancel
-		mongo.connect(mongoDBUrl, function(err, client) {
-			const db = client.db(database);	
-			if (err) throw err;
-		
+		db.collection('users').update(
+			{ username: req.session.username },
+			{ $set: { email: req.body.accountEmail }}
+		);
+
+		res.json({
+			accountModified: true,
+			accountMsg: 'Account Modified!',
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			accountModified: false,
+			accountMsg: 'Error modifying account',
+		});
+	}
+});
+
+app.post('/api/accountLoginModify', requireLogin, function(req, res) {
+	try {
+		db.collection('users').findOne({
+			username: req.session.username
+		});
+		if (item.password !== encrypt(req.body.accountOldPassword)) {
+			res.json({
+				pwordChangeMsg: 'Old Password Incorrect!',
+			});
+		} else {
 			db.collection('users').update(
 				{ username: req.session.username },
-				{ $set: { email: req.body.accountEmail }},
-				function(err) {
-					if (err) throw err;
-
-					res.json({
-						accountModified: true,
-						accountMsg: 'Account Modified!',
-					});
-				}
-			);
-		});
-	}
-});
-
-app.post('/api/accountLoginModify', function(req, res) {
-	if (req.session.username) {
-		mongo.connect(mongoDBUrl, function(err, client) {
-			const db = client.db(database);	
-			if (err) throw err;
-			
-			db.collection('users').findOne({
-				username: req.session.username
-			}, function(err, item) {
-				if (err) throw err;
-				
-				if (item.password !== encrypt(req.body.accountOldPassword)) {
-					res.json({
-						pwordChangeMsg: 'Old Password Incorrect!',
-					});
-				}
-				else {
-					db.collection('users').update(
-						{ username: req.session.username },
-						{ $set:
-							{
-								password: encrypt(req.body.accountPassword)
-							}
-						}, function(err) {
-							if (err) throw err;
-
-							res.json({
-								pwordChangeMsg: 'Password Changed!',
-							});
-						}
-					);
-				}
-			});
-		});
-	}
-});
-
-app.get('/api/progress', function(req, res) {
-	if (req.session.username) {
-		mongo.connect(mongoDBUrl, function(err, client) {
-			const db = client.db(database);	
-			if (err) throw err;
-			
-			db.collection('meditationrecord').find({
-				$and: [
+				{ $set:
 					{
-						username: {
-							$eq: req.session.username
-						}
-					},
-					{
-						meditateDateTime: {
-							$lte: Number(req.query.endTimestamp),
-						}
-					},
-					{
-						meditateDateTime: {
-							$gte: Number(req.query.startTimestamp),
-						}
+						password: encrypt(req.body.accountPassword)
 					}
-				]
-			}).toArray(function(err, meditationRecords) {
-				if (err) throw err;
-
-				res.json({
-					meditationRecords,
-					recordsFound: true,
 				});
+			res.json({
+				pwordChangeMsg: 'Password Changed!',
 			});
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			pwordChangeMsg: 'Error changing password',
+		});
+	}
+});
+
+app.get('/api/progress', requireLogin, function(req, res) {
+	try {
+		const meditationRecords = db.collection('meditationrecord').find({
+			$and: [
+				{
+					username: {
+						$eq: req.session.username
+					}
+				},
+				{
+					meditateDateTime: {
+						$lte: Number(req.query.endTimestamp),
+					}
+				},
+				{
+					meditateDateTime: {
+						$gte: Number(req.query.startTimestamp),
+					}
+				}
+			]
+		}).toArray();
+
+		res.json({
+			meditationRecords,
+			recordsFound: true,
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			recordsFound: false,
+			meditationRecords: [],
 		});
 	}
 });
 
 // Modify the journal entry in the collection, using the id to find it
-app.post('/api/modifyJournalEntry', function(req, res) {
-	if (req.session.username) {
+app.post('/api/modifyJournalEntry', requireLogin, function(req, res) {
+	try {
 		const jeid = new OID(req.body.journalID);
 
 		// Update the record
-		mongo.connect(mongoDBUrl, function(err, client) {
-			const db = client.db(database);	
-			if (err) throw err;
-			
-			db.collection('meditationrecord').update(
-				{ _id: jeid },
-				{ $set:
-					{ journalEntry: req.body.journalEntry }
-				},
-				function(err, docs) {
-					if (err) throw err;
-
-					res.json({
-						journalModified: true,
-					});
-				}
-			);
+		db.collection('meditationrecord').update(
+			{ _id: jeid },
+			{ $set:
+				{ journalEntry: req.body.journalEntry }
+			}
+		);
+		res.json({
+			journalModified: true,
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			journalModified: false,
 		});
 	}
 });
 
 // Delete a journal entry
-app.post('/api/deleteJournalEntry', function(req, res) {
-	if (req.session.username) {
+app.post('/api/deleteJournalEntry', requireLogin, function(req, res) {
+	try {
 		const jdid = new OID(req.body.journalID);
 
 		// Delete the record
-		mongo.connect(mongoDBUrl, function(err, client) {
-			const db = client.db(database);	
-			if (err) throw err;
-			
-			db.collection('meditationrecord').remove({
-				_id: jdid
-			}, function(err) {
-				if (err) throw err;
+		db.collection('meditationrecord').remove({
+			_id: jdid
+		});
 
-				res.json({
-					journalDeleted: true,
-				});
-			});
+		res.json({
+			journalDeleted: true,
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			journalDeleted: false,
 		});
 	}
 });
